b92246dd6d7cbab9dab0ac8dcb3517418ba4d8e0
Andrea Susnick <asusnick@chathamfinancial.com>
2021-04-03 09:11:04 -0400

    refactor to user input class

--- src/qaengineerproject/Main.java
+++ src/qaengineerproject/Main.java
@@ -4,30 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //declarations
-        int smallDogs, mediumDogs, largeDogs;
-
-        int lbsFoodRemainingEOM;
-
-        //create scanner
-        Scanner scanner = new Scanner(System.in);
-
-        //display prompt
-        System.out.print("Please enter number of small dogs: ");
-        smallDogs = scanner.nextInt();
-        System.out.print("Please enter number of medium dogs: ");
-        mediumDogs = scanner.nextInt();
-        System.out.print("Please enter number of large dogs: ");
-        largeDogs = scanner.nextInt();
-        System.out.print("Please enter lbs of food remaining: ");
-        lbsFoodRemainingEOM = scanner.nextInt();
-
-        //calculations
-        Calculations.CalculateDogSizeFoodNeeds(smallDogs, mediumDogs, largeDogs);
-        Calculations.CalculateNetLbsEOM(lbsFoodRemainingEOM);
-        var amountOfFoodToOrder = Calculations.CalculateTotalAmountOfFoodToOrder();
-
-        //results
-        System.out.println("The amount of food you need to order: " + amountOfFoodToOrder + "lbs");
+        UserInput.UserInput();
     }
 }
